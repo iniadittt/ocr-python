7a9a1e50b3c49b7dc857e3bf729ace008f5fe485
iniadittt <aditya.braincore@gmail.com>
2024-10-03 19:30:44 +0700

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import os
+import re
 import gdown
+import pdf2image
+import pytesseract
 from datetime import datetime
-from extraction import Extrac     
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+def extract_text_from_pdf(pdf_path):
+    try:
+        pages = pdf2image.convert_from_path(pdf_path, 500)
+        text_data = ''
+        for page in pages:
+            text = pytesseract.image_to_string(page)
+            text_data += text + '\n'
+        return text_data
+    except Exception as e:
+        print(f"Error processing PDF: {e}")
+        return ''
+
+def extract_email_and_phone(text):
+    email_pattern = r'[a-zA-Z0-9._%+-]+\s*[a-zA-Z0-9._%+-]*\s*@\s*[a-zA-Z0-9.-]+\s*\.\s*(?:[a-zA-Z]{2,}|ac\.id|co\.id|go\.id|org\.id|net\.id|sch\.id|mil\.id|info|biz|xyz|me|online|tech|store|site|cloud)'
+    phone_pattern = r'\b(?:\+?62|62|08)\d{2,3}[-\s]?\d{3,4}[-\s]?\d{3,4}\b'
+    emails = re.findall(email_pattern, text)
+    phones = re.findall(phone_pattern, text)
+    cleaned_emails = [re.sub(r'\s*@\s*', '@', email) for email in emails]
+    return cleaned_emails, phones
 
 @app.route("/", methods=['GET'])
 def main():
@@ -22,17 +44,14 @@ def ocr():
         results = []
         for filename in os.listdir(pdf_folder):
             if filename.endswith('.pdf'):
-                print(filename)
                 pdf_path = os.path.join(pdf_folder, filename)
-                text = Extrac.extract_text_from_pdf(pdf_path)
-                emails, phones = Extrac.extract_email_and_phone(text)
-                print(text)
-                print(emails)
-                print(phones)
+                text = extract_text_from_pdf(pdf_path)
+                emails, phones = extract_email_and_phone(text)
                 results.append({
+                    "text": text,
                     "filename": filename,
-                    "emails": emails,
-                    "phones": phones
+                    "emails": emails[0] if emails else None, 
+                    "phones": phones[0] if phones else None
                 })
         return jsonify({
             "statusCode": 200,
